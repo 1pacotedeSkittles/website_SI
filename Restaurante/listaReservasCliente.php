18da880802418dfc8dcf31b9b5a89af2ad8850c3
Rafael.Alves.Campos <rafaelcampos@student.dei.uc.pt>
2025-12-03 15:53:26 +0000

    .html to .php

---
+++ Restaurante/listaReservasCliente.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>eatEasy - Lista de Reservas</title>
+</head>
+<body>
+
+<header>
+    <nav>
+        <div>
+            <a href="index.html"><strong>eatEasy</strong></a>
+        </div>
+
+        <div>
+            <input type="search" placeholder="Pesquisar...">
+        </div>
+
+        <div>
+            <a href="contactos.html">Contactos</a>
+        </div>
+    </nav>
+</header>
+
+<main>
+
+    <section id="lista-reservas">
+        <h2>As suas reservas</h2>
+
+        <div id="cartoes-reservas">
+
+            <article>
+                <img src="imagens/reserva_restaurante1.jpg" alt="Foto Restaurante 1" width="250" height="180">
+                <div>
+                    <h3>Nome restaurante</h3>
+                    <ul>
+                        <li>Data (d/m/a)</li>
+                        <li>Hora (00h:00m.)</li>
+                        <li>Estado do reserva: **confirmado**</li>
+                    </ul>
+                </div>
+            </article>
+
+            <article>
+                <img src="imagens/reserva_restaurante2.jpg" alt="Foto Restaurante 2" width="250" height="180">
+                <div>
+                    <h3>Nome restaurante</h3>
+                    <ul>
+                        <li>Data (d/m/a)</li>
+                        <li>Hora (00h:00m.)</li>
+                        <li>Estado do reserva: **confirmado**</li>
+                    </ul>
+                </div>
+            </article>
+
+            <article>
+                <img src="imagens/reserva_restaurante3.jpg" alt="Foto Restaurante 3" width="250" height="180">
+                <div>
+                    <h3>Nome restaurante</h3>
+                    <ul>
+                        <li>Data (d/m/a)</li>
+                        <li>Hora (00h:00m.)</li>
+                        <li>Estado do reserva: **cancelado**</li>
+                    </ul>
+                </div>
+            </article>
+
+            <article>
+                <img src="imagens/reserva_restaurante4.jpg" alt="Foto Restaurante 4" width="250" height="180">
+                <div>
+                    <h3>Nome restaurante</h3>
+                    <ul>
+                        <li>Data (d/m/a)</li>
+                        <li>Hora (00h:00m.)</li>
+                        <li>Estado do reserva: **confirmado**</li>
+                    </ul>
+                </div>
+            </article>
+
+        </div>
+    </section>
+
+    <aside id="calendario-resumo">
+        <h3>Calendário</h3>
+
+        <fieldset>
+            <legend>Calendário</legend>
+            <label for="data">Data:</label>
+            <input type="date" id="data" name="data" required>
+        </fieldset>
+    </aside>
+
+</main>
+
+<footer>
+    <hr>
+    <div>
+        <a href="#">sobre nós</a> | <a href="#">termos de utilização</a>
+        <span> | 📷 f X</span>
+    </div>
+    <div>
+        <small>© 2025 eatEasy. todos os direitos reservados.</small>
+    </div>
+</footer>
+
+</body>
+</html>
---
+++ Restaurante/loginAdmin.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>eatEasy</title>
+</head>
+<body>
+<header>
+    <nav>
+        <div>
+            <a href="index.html"> <img src="../Logo/eatEasy.png" height="32" width="149" alt="logo"/></a>
+        </div>
+
+        <ul id="menu-navegacao">
+            <li><a href="contactos.html">Contactos</a></li>
+            <li><a href="login.html">Login</a></li>
+            <li><a href="loginRegisto.html">Registar</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+    <div class="Login-Admin">
+        <div class="logo">eatEasy</div>
+
+        <div class="login-box">
+            <h1 class="login-title">Login</h1>
+            <form action="#" method="POST" class="login-form">sdad
+
+                <input type="text" name="nome" placeholder="Nome" required class="input-field">
+
+                <input type="password" name="password" placeholder="Palavra-Passe" required class="input-field">
+
+                <div class="create-account-link-container">
+                    <a href="#" class="create-account-link">Criar conta</a>
+                </div>
+
+                <div id="botao-entrar">
+                    <a href="inicialAdmin.html" class="entrar">Entrar</a>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</main>
+
+<footer class="login-para-admin">
+    <a href="#" class="admin-link">Administrador</a>
+</footer>
+
+</body>
+</html>
