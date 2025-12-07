a80c24b38edb98f740bba4f6deb3ddab6c654424
uma_garrafa_de_azeite <diogofsilva@student.dei.uc.pt>
2025-12-07 17:44:04 +0000

    correção dos caminhos para ir buscar as imagens dos logos das redes sociais no footer

--- Restaurante/ficheiro/php/includes/footer.php
+++ Restaurante/ficheiro/php/includes/footer.php
@@ -4,15 +4,15 @@
         <p>Termos de utilização</p>
         <!--icones redes-->
         <a href="https://www.instagram.com/cristiano/" class="instagram-logo-link" target="_blank">
-            <img src="redes_sociais/instagram_1.png" alt="logo instagram"/>
+            <img src="../../../redes_sociais/instagram_1.png" alt="logo instagram"/>
         </a>
 
         <a href="https://www.instagram.com/cristiano/" class="facebook-logo-link" target="_blank">
-            <img src="redes_sociais/facebook_1.png" alt="logo facebook"/>
+            <img src="../../../redes_sociais/facebook_1.png" alt="logo facebook"/>
         </a>
 
         <a href="https://www.instagram.com/cristiano/" class="x-logo-link" target="_blank">
-            <img src="redes_sociais/x_1.png" alt="logo x"/>
+            <img src="../../../redes_sociais/x_1.png" alt="logo x"/>
         </a>
     </div>
 </footer>
