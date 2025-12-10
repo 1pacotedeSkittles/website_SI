9a3fc61f37bc44221768ae4bb8cde53cde43cb8d
uma_garrafa_de_azeite <diogofsilva@student.dei.uc.pt>
2025-12-10 12:40:11 +0000

    added code to config.php

--- Restaurante_2/config/config.php
+++ Restaurante_2/config/config.php
@@ -1,1 +1,15 @@
 <?php
+function getDBConnection()  /*criação de uma função para a conexão*/
+{
+    $str = "dbname=postgres user=postgres password=postgres host=localhost port=5432";
+    $connection = pg_connect($str);
+
+    if (!$connection) {
+        die("Erro na ligacao");
+    }
+
+    echo "Ligacao estabelecida!";
+// do something here
+    pg_close($connection);
+}
+
