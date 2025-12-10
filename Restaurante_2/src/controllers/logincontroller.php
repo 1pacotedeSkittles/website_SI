119b1a8e96b1a89caa3fd653a8fcea4d46f6a810
uma_garrafa_de_azeite <diogofsilva@student.dei.uc.pt>
2025-12-10 19:37:26 +0000

    added some files

---
+++ Restaurante_2/src/controllers/logincontroller.php
@@ -0,0 +1,5 @@
+<?php
+require_once '../../config/database.php';
+$connection=getDBConnection();
+$username = $_POST['username'];
+$password = $_POST['password'];
--- Restaurante_2/src/views/login.view.php
+++ Restaurante_2/src/views/login.view.php
@@ -1,1 +1,16 @@
-<?php
+<!-- Formulário de login -->
+<form action="../action/processa.login.php" method="POST" class="login-form">
+
+    <input type="text" name="nome_ou_email" placeholder="Nome/ID ou Email" required class="input-field">
+
+    <input type="password" name="password" placeholder="Palavra-Passe" required class="input-field">
+
+    <div class="create-account-link-container">
+        <a href="Registo.php" class="create-account-link">Criar conta</a>
+    </div>
+
+    <div id="botao-entrar">
+        <button type="submit" class="entrar">Entrar</button>
+    </div>
+
+</form>
