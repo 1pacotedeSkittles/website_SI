4b8f9df3c012508f5b04804f3d4f58e239c4e1d2
uma_garrafa_de_azeite <diogofsilva@student.dei.uc.pt>
2025-12-11 10:00:04 +0000

    corrected navbar

--- Restaurante/ficheiro/php/includes/navbar.php
+++ Restaurante/ficheiro/php/includes/navbar.php
@@ -11,7 +11,5 @@
             <li><a href="../pages/contactos.php">Contactos</a></li>
         </ul>
 
-        <span id="abrirPainel" class="nome-cliente"><?= $cliente['nome'] ?></span>
-
     </nav>
 </header>
