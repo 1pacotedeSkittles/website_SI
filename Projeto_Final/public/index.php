ff5304cd315ccb4e1049c92996c47c89e49729fb
uma_garrafa_de_azeite <diogofsilva@student.dei.uc.pt>
2025-12-11 17:57:41 +0000

    change index.php

--- Projeto_Final/public/index.php
+++ Projeto_Final/public/index.php
@@ -1,1 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>eatEasy</title>
+
+    <!-- Font -->
+    <link rel="stylesheet" href="https://use.typekit.net/zcu4bcg.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/footer.css">
+</head>
+
+<body>
+<?php include '../includes/header.php'; ?>
+
+<?php include '../src/views/index.view.php'; ?>
+
+<?php include '../includes/footer.php'; ?>
+</body>
+</html>
