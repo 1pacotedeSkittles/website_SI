50b74fa12b190626833c1cfd0d9c94aa2cca7c15
uma_garrafa_de_azeite <diogofsilva@student.dei.uc.pt>
2025-12-11 20:35:53 +0000

    added files

--- Projeto_Final/public/login.php
+++ Projeto_Final/public/login.php
@@ -1,1 +1,27 @@
 <?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.typekit.net/zcu4bcg.css">
+    <title>eatEasy</title>
+
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/footer.css">
+</head>
+<body>
+
+<?php include '../includes/header.php'; ?>
+<?php include '../src/views/login.view.php'; ?>
+<?php include '../includes/footer.php'; ?>
+
+
+
+</body>
+</html>
+
--- Projeto_Final/src/views/login.view.php
+++ Projeto_Final/src/views/login.view.php
@@ -1,1 +1,19 @@
-<?php
+<main>
+    <div class="auth-container">
+        <div class="auth-box">
+            <h1 class="auth-title">Login</h1>
+
+            <form action="../controllers/logincontroller.php" method="POST" class="login-form">
+                <input class="input-field" type="username_ou_email" name="username_ou_email" placeholder="username_ou_email" required>
+                <input class="input-field" type="password" name="password" placeholder="Palavra-Passe" required>
+
+                <div class="aux-links">
+                    <a href="loginRegisto.php">Criar conta</a>
+                </div>
+
+                <button class="action-btn" type="submit">Entrar</button>
+            </form>
+
+        </div>
+    </div>
+</main>
