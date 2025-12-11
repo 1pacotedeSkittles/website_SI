d3fa0c003edd39bb711a573a60a5e436db7394ca
uma_garrafa_de_azeite <diogofsilva@student.dei.uc.pt>
2025-12-11 15:56:49 +0000

    added changes

--- Projeto_Final/config/config.php
+++ Projeto_Final/config/config.php
@@ -1,1 +1,17 @@
 <?php
+function getDBConnection()  /*criação de uma função para a conexão*/
+{
+    $str = "dbname=postgres user=postgres password=postgres host=localhost port=5432";
+    $connection = pg_connect($str);
+
+    if (!$connection) {
+        die("Erro na ligacao");
+    }
+
+    // echo "Ligacao estabelecida!";
+    // do something here
+    return $connection;
+}
+
+// Cria a conexão global
+$conn = getDBConnection();
