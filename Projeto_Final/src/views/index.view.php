80588de902d221a1725d4ca460cb78da58e36242
uma_garrafa_de_azeite <diogofsilva@student.dei.uc.pt>
2025-12-11 19:48:29 +0000

    added code for index.view.php

--- Projeto_Final/src/views/index.view.php
+++ Projeto_Final/src/views/index.view.php
@@ -1,1 +1,47 @@
-<?php
+<main id="restaurantes">
+    <div>
+        <h1>Faça já a sua reserva</h1>
+    </div>
+
+    <article>
+        <div>
+            <img src="R1/interior.jpg"/>
+        </div>
+        <div>
+            <h3>Restaurante 1</h3>
+            <p>localização</p>
+            <p>Tipo de comida</p>
+            <p>Preço médio por prato</p>
+        </div>
+
+        <div>
+            <img src="../../../fotos/foto.webp" height="800" width="600" alt="foto2"/>
+        </div>
+        <div>
+            <h3>Restaurante 2</h3>
+            <p>localização</p>
+            <p>Tipo de comida</p>
+            <p>Preço médio por prato</p>
+        </div>
+
+        <div>
+            <img src="../../../fotos/foto.webp" height="800" width="600" alt="foto3"/>
+        </div>
+        <div>
+            <h3>Restaurante 3</h3>
+            <p>localização</p>
+            <p>Tipo de comida</p>
+            <p>Preço médio por prato</p>
+        </div>
+
+        <div>
+            <img src="../../../fotos/foto.webp" height="800" width="600" alt="foto4"/>
+        </div>
+        <div>
+            <h3>Restaurante 4</h3>
+            <p>localização</p>
+            <p>Tipo de comida</p>
+            <p>Preço médio por prato</p>
+        </div>
+    </article>
+</main>
