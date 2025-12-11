a243576f18c8c48832784f10f4bf6426d40a688b
uma_garrafa_de_azeite <diogofsilva@student.dei.uc.pt>
2025-12-11 10:56:58 +0000

    added changes

--- Restaurante_2/public/login.php
+++ Restaurante_2/public/login.php
@@ -1,13 +1,13 @@
 <?php
+
+// login.php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
+// ...
 
 $title = 'eateasy';
-$content = '../src/views/index.view.php';
-
-// Se já está logado, redirecionar
-if (isset($_SESSION['user_id'])) {
-    header('Location: index.php');
-    exit();
-}
+$content = '../src/views/login.view.php';
 require '../includes/master.php';
-?>
--- Restaurante_2/src/views/login.view.php
+++ Restaurante_2/src/views/login.view.php
@@ -1,12 +1,5 @@
-<?php
-// Mostrar mensagem de erro se houver
-if (isset($_GET['erro'])) {
-    echo '<p style="color: red;">Credenciais inválidas! Tenta novamente.</p>';
-}
-?>
-
 <!-- Formulário de login -->
-<form action="../src/controllers/logincontroller.php" method="POST" class="login-form">
+<form action="../controllers/logincontroller.php" method="POST" class="login-form">
     <input class="input-field" type="text" name="email" placeholder="email" required>
     <input class="input-field" type="password" name="password" placeholder="Palavra-Passe" required>
 
